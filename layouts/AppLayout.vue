34b9008512441b4416362421016ce0e5a74d65ea
Iraklis Asimakopoulos <i.asimakopoulos@dataverse.gr>
2025-04-04 00:46:08 +0300

    feat: created App Layout

---
+++ layouts/AppLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col items-center sm:p-[20px]">
+    <AppHeader />
+    <div
+      class="m-auto flex flex-col sm:flex-row w-full max-w-[1512px] gap-[16px] sm:gap-[20px]"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
